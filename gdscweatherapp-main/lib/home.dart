ece69fd32768f3e23cdd821468b8c6c87a3f6f71
vanshika704 <sharmavanshi704@gmail.com>
2023-12-01 10:00:00 +0530

    body

--- gdscweatherapp-main/lib/home.dart
+++ gdscweatherapp-main/lib/home.dart
@@ -34,6 +34,7 @@ class _homeState extends State<home> {
             Row(children: [Text("1:00"), Text("32"),Icon(Icons.sunny)],),
             Row(children: [Text("2:00"), Text("33"),Icon(Icons.sunny)],),
             Row(children: [Text("3:00"), Text("32"),Icon(Icons.sunny)],),
+             Row(children: [Text("4:00"), Text("32"),Icon(Icons.sunny)],),
 
            
         ],
